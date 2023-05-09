39239a78e34dfdb63f2690a741a7cbcaf5fd0235
angelsoempty <denatamanov1@gmail.com>
2023-05-09 03:06:17 +0300

    №4

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
-    def info(self):
-        print(f"Марка: {self.brand}, Модель: {self.model}, Рік: {self.year}")
+class Bank:
+    def __init__(self):
+        self.customers = []
+    def add_customer(self, customer):
+        self.customers.append(customer)
+    def get_total_balance(self):
+        total_balance = 0
+        for customer in self.customers:
+            total_balance += customer.get_balance()
+        return total_balance
 
-car = Car('Mercedes', 'W223','2020')
-car.info()
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def get_balance(self):
+        return self.balance
+
+bank = Bank()
+
+customer1 = BankAccount(1000)
+customer2 = BankAccount(2000)
+customer3 = BankAccount(3000)
+
+bank.add_customer(customer1)
+bank.add_customer(customer2)
+bank.add_customer(customer3)
+
+total_balance = bank.get_total_balance()
+print(total_balance)
